efef694bd109485cb78003562cc15040726a8645
Daniel Knaub <knaubdev@gmail.com>
2022-10-27 20:59:41 +0800

    Some changes

--- password_generator/settings.py
+++ password_generator/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'rhc2z#r1tc&67g&*rx3%&+wrk+r-s#6(v56ph3&5fx_5=7l980'
+SECRET_KEY = 'rhc2z#r1tc&67dg&*rx3%&+wrk+r-s#6(v56ph3&5fx_5=7l980'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
